4b5cb11f6c983a7810f48ec5105ab4d5929d5866
André Dal Corsi <andredalcorsi1@gmail.com>
2025-01-25 12:40:22 -0300

    teste

--- cap02/exemplos/ex2.2/ex2.2.js
+++ cap02/exemplos/ex2.2/ex2.2.js
@@ -10,5 +10,5 @@ form.addEventListener("submit", (e) => {
     let movieInHours = Math.floor(movieDuration / 60)
     let movieRest = (movieDuration % 60)
     resp.innerText = `Nome do filme: ${movieN}. Duração: ${movieInHours} hora(s) e ${movieRest} minuto(s). ` 
-    e.preventDefault() 
+    e.preventDefault()
 })
