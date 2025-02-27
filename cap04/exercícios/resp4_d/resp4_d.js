503fb800ebbceaef5ea10a3b2e2d21295626d9c3
André Dal Corsi <andredalcorsi1@gmail.com>
2025-02-27 00:32:17 -0300

    resolução do exercício final do cap04

---
+++ cap04/exercícios/resp4_d/resp4_d.js
@@ -0,0 +1,29 @@
+/* Elaborar um programa que leia três lados e verifiique se eles podem ou não formar um triângulo. 
+Para formar um triângulo, um dos lados não pode seer maior que a soma dos outros doisi. Caso 
+possam formar um triângulo, exiba também qual o tipo do triângulo.
+
+Equilátero: 03 lados iguais 
+Isósceles: 02 lados iguais
+Escaleno: 03 lados diferentes */
+
+const form = document.querySelector('form')
+const resp = document.querySelector('#resp')
+
+form.addEventListener("submit", (e) => { 
+    e.preventDefault()
+
+    const sizeOne = Number(form.sizeOne.value)
+    const sizeTwo = Number(form.sizeTwo.value)
+    const sizeThree = Number(form.sizeThree.value) 
+
+    if (sizeOne === sizeTwo && sizeTwo === sizeThree && sizeThree === sizeOne) {
+        resp.textContent = `Equilátero`
+    } else if (sizeOne === sizeTwo || sizeTwo === sizeThree || sizeThree === sizeOne) { 
+        resp.textContent = `Isósceles`
+    } else if (sizeOne != sizeTwo && sizeTwo != sizeThree && sizeThree != sizeOne) { 
+        resp.textContent = `Escaleno`
+    } else { 
+        alert ('Favor, colocar um valor válido.')
+    }
+
+})
