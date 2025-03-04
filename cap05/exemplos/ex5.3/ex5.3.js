172844b20897f03e7ab4f2d6f0164a5ae1d799ce
André Dal Corsi <andredalcorsi1@gmail.com>
2025-03-04 12:53:31 -0300

    resolução de exemplos do cap05

--- cap05/exemplos/ex5.3/ex5.3.js
+++ cap05/exemplos/ex5.3/ex5.3.js
@@ -1,23 +1,15 @@
-const form = document.querySelector('form')
+let num 
+do {
+    num = Number(prompt("Insira um número: "))
 
-form.addEventListener('submit', (e => { 
-    e.preventDefault()
+    if (num == 0 || isNaN(num)) {
+        alert("Digite um número válido...")
+    }
+} while (num == 0 || isNaN(num)) 
+    let pares = `Pares entrre 1 e ${num}: `
 
-    let num 
-    
-    do { 
-        num = Number(prommpt("Numero: ")) // lê um número
-        if (num == 0 || isNaN(num)) {
-            alert("Digite um número válido")
-        } while (num == 0 || isNaN(num)){
-            
-            let pares = `Pares entre 1 e ${num}:`
-
-            for (let i = 2; i <= num; i = i + 2)
-
-            pares = pares + i + ","
-        }
-        alert(pares)       
+    for (let i=2; i <= num; i = i + 2) {
+        pares = pares + i 
     }
 
-}))
+    alert(pares)
